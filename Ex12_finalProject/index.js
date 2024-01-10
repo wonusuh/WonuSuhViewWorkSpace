56bd5a44f62d086b7aebb0bd2d05155cc0865577
wonusuh <wonwooseo23@gmail.com>
2024-01-10 18:45:09 +0900

    my first complete view

---
+++ Ex12_finalProject/index.js
@@ -0,0 +1,46 @@
+$('.myslider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+$(window).scroll(
+  function () {
+    if ($(this).scrollTop() > 1) {
+      $('nav, .gnb li a').addClass('active');
+    } else {
+      $('nav, .gnb li a').removeClass('active');
+    }
+  }
+);
